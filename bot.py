2cf93017d3c8be718d519bac5641e225901b5a6c
Grant Kurtz <kurtz.grant@gmail.com>
2020-09-17 17:19:39 -0400

    Refactored as a stateful bot, much improved performance.
    * Created Guild class to store roles to avoid unnecessary lookups and resetting of roles
    * Crewmate role is only removed from those that get the Imposter role
    * We check for user's roles and store them as if they had played before starting the next game. This helps makes sure we pick up new people with the "Among Us" role and to cache the roles they were previously or manually given.
    * Total speed up: 100x. Went from 60s to 600ms to start a new game.

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import discord
-import random
 import asyncio
+from timeit import default_timer as timer
+
+import guild
 
 client = discord.Client()
+guilds = {}
 
 
 @client.event
@@ -12,98 +15,40 @@ async def on_ready():
     """
     print('We have logged in as {0.user}'.format(client))
     print("Number of guilds in: " + str(len(client.guilds)))
+    for g in client.guilds:
+        server = guild.Guild(g)
+        guilds[g.id] = server
+
+    await asyncio.gather(*(guilds[i].setup() for i in guilds))
+    print("Initial loading complete")
 
 
 @client.event
 async def on_message(message):
-    crewmate_role = imposter_role = among_us_role = None
+
+    await client.wait_until_ready()
+
+    # don't want to trigger ourself by accident
     if message.author == client.user:
         return
 
     if message.content.startswith('who among us'):
         print("Starting a new round of Among Us in '" + message.channel.guild.name + "'")
         print("Channel from: " + str(message.channel))
-
         print("Members in channel: " + str(len(message.channel.members)))
+        server = guilds[message.channel.guild.id]
+        start = timer()
+        await server.new_game()
+        print("Game prepared in: " + str(int((timer() - start) * 1000)) + " milliseconds")
 
-        print("Roles in server: " + str(len(message.channel.guild.roles)))
-        for r in message.channel.guild.roles:
-            print(str(r))
-            if r.name == "Imposter":
-                print("Found Imposter role")
-                imposter_role = r
-            elif r.name == "Crewmate":
-                print("Found Crewmate role")
-                crewmate_role = r
-            elif r.name == "among us":
-                print("Found Among Us role")
-                among_us_role = r
-
-        if among_us_role is None:
-            print("No Among Us role to use, ignoring request")
-            return
-
-        # Create the roles if they don't exist
-        if imposter_role is None:
-            print("Creating imposter role")
-            color = discord.colour.Color.dark_red()
-            imposter_role = await message.channel.guild.create_role(name="Imposter", color=color)
-        if crewmate_role is None:
-            print("Creating crewmate role")
-            color = discord.colour.Color.dark_blue()
-            crewmate_role = await message.channel.guild.create_role(name="Crewmate", color=color)
-
-        # List of members who are playing Among Us
-        members = []
-        for m in message.channel.members:
-            if m.bot:
-                continue
-            elif among_us_role in m.roles:
-                members.append(m)
-
-        # Don't start a game if too few people have this role.
-        # if len(members) < 3:
-        #     print("Too few people to start")
-        #     await message.channel.send('There are too few for imposters to be Among Us...right?')
-        #     return
-        print("There are " + str(len(members)) + " playing")
-
-        # For now, 20% of those with the role will be imposters, and at least one person will be an imposter
-        imposters = int(len(members) * 0.2)
-        if imposters < 1:
-            imposters = 1
-        print("There will be " + str(imposters) + " imposters")
-
-        # Assign roles to members randomly
-        random.shuffle(members)
-        new_imposters = 0
-
-        # remove_roles = []
-        # for m in members:
-        #     remove_roles.append(m.remove_roles(imposter_role, crewmate_role))
-        res = await asyncio.gather(*(m.remove_roles(imposter_role, crewmate_role) for m in members))
-        print(res)
-
-        await asyncio.gather(*(m.add_roles(imposter_role) for m in members[:imposters]))
-        print("Done assigning imposters")
-        await asyncio.gather(*(m.add_roles(crewmate_role) for m in members[imposters:]))
-        print("Done assigning crewmates")
-
-        # for m in members:
-        #     if new_imposters < imposters:
-        #         print(m.display_name + " is an imposter")
-        #         await m.add_roles(imposter_role)
-        #         new_imposters += 1
-        #     else:
-        #         await m.add_roles(crewmate_role)
-        #         print(m.display_name + " is a crewmate")
-        print("Done assigning roles")
+        print("There are " + str(len(server.players)) + " playing")
         await message.channel.send(
-            "There are {0} crewmates and {1} imposters Among Us!".format(str(len(members) - imposters), str(imposters)))
+            "There are {0} crewmates and {1} imposters Among Us!".format(str(len(server.crewmates)),
+                                                                         str(len(server.imposters))))
 
 
-# Blocking function call, nothing will run after this
 token_file = open("bot_token.txt")
 res = token_file.read()
-print(res)
+
+# Blocking function call, nothing will run after this
 client.run(res)
---
+++ guild.py
@@ -0,0 +1,78 @@
+import random
+
+import discord
+import asyncio
+
+
+class Guild:
+    players = []
+    imposters = []
+    crewmates = []
+    guild = None
+    imposter_role = None
+    crewmate_role = None
+    among_us_role = None
+
+    def __init__(self, guild):
+        self.guild = guild
+
+    async def setup(self):
+        print("\t\t==== Setting up: " + self.guild.name + " ====")
+
+        for r in self.guild.roles:
+            if r.name == "Imposter":
+                print("Found Imposter role: " + str(r) + " id: " + str(r.id))
+                self.imposter_role = r
+            elif r.name == "Crewmate":
+                print("Found Crewmate role: " + str(r) + " id: " + str(r.id))
+                self.crewmate_role = r
+            elif r.name == "among us":
+                print("Found Among Us role: " + str(r) + " id: " + str(r.id))
+                self.among_us_role = r
+
+        if self.among_us_role is None:
+            print("No Among Us role to use, ignoring request")
+            return
+
+        # Create the roles if they don't exist
+        if self.imposter_role is None:
+            print("Creating imposter role")
+            self.imposter_role = await self.guild.create_role(name="Imposter", color=discord.colour.Color.dark_red())
+        if self.crewmate_role is None:
+            print("Creating crewmate role")
+            self.crewmate_role = await self.guild.create_role(name="Crewmate", color=discord.colour.Color.dark_blue())
+
+        self.find_players()
+
+    async def new_game(self):
+        self.find_players()
+        num_imposters = int(len(self.players) * 0.2)
+        if num_imposters < 1:
+            num_imposters = 1
+
+        # reset the roles of the previous imposters
+        if len(self.imposters) > 0:
+            await asyncio.gather(*(m.add_roles(self.crewmate_role) for m in self.imposters))
+            await asyncio.gather(*(m.remove_roles(self.imposter_role) for m in self.imposters))
+        else:
+            await asyncio.gather(*(m.add_roles(self.crewmate_role) for m in self.players))
+
+        random.shuffle(self.players)
+        self.imposters.clear()
+        self.crewmates.clear()
+        await asyncio.gather(*(m.add_roles(self.imposter_role) for m in self.players[:num_imposters]))
+
+        for m in self.players[:num_imposters]:
+            self.imposters.append(m)
+        for m in self.players[num_imposters:]:
+            self.crewmates.append(m)
+
+    def find_players(self):
+        for m in self.guild.members:
+            if self.among_us_role in m.roles and m not in self.players:
+                self.players.append(m)
+
+            if self.crewmate_role in m.roles and m not in self.crewmates:
+                self.crewmates.append(m)
+            if self.imposter_role in m.roles and m not in self.imposters:
+                self.imposters.append(m)
