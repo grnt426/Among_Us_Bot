05f74c8d1ebe8fdde090ff29b6e72da7e493e790
Grant Kurtz <kurtz.grant@gmail.com>
2020-09-23 10:31:26 -0400

    Fixed guild.py to properly use Python objects, added logging.
    * Basic logging configuration added for now to preserve issues in a file.
    * More exception handling needed
    * I had a woeful misunderstanding of how objects work in Python. That has been corrected and now everything isn't shared among all instances of objects
    * Will need to clean up logging to use a separate handler so I can appropriately suppress the discord library's DEBUG logs while showing my own. This would allow me to then use DEBUG level in future commits.

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
-import discord
 import asyncio
+import logging
 from timeit import default_timer as timer
+import discord
 
 import guild
+
+logging.basicConfig(filename='bot.log', format='%(asctime)s [%(levelname)s] %(message)s',
+                    filemode='w', level=logging.INFO)
 
 client = discord.Client()
 guilds = {}
@@ -14,15 +18,19 @@ async def on_ready():
     Called whenever it connects to Discord and has populated the internal cache
     """
 
-    print('We have logged in as {0.user}'.format(client))
-    print("Number of guilds in: " + str(len(client.guilds)))
+    logging.info('We have logged in as {0.user}'.format(client))
+    logging.info("Number of guilds in: {0} ".format(len(client.guilds)))
     for g in client.guilds:
         if g.id not in guilds:
             server = guild.Guild(g)
+            logging.info(server)
             guilds[g.id] = server
 
     await asyncio.gather(*(guilds[i].setup() for i in guilds))
-    print("Initial loading complete")
+    logging.info("Initial loading complete")
+
+    for s in guilds:
+        logging.info("{0} players in {1}".format(len(guilds[s].players), s))
 
 
 @client.event
@@ -37,30 +45,31 @@ async def on_message(message):
     if message.content.startswith('who among us'):
         server = guilds[message.channel.guild.id]
 
-        print("Starting a new round of Among Us in '" + message.channel.guild.name + "'")
-        print("Channel from: " + str(message.channel))
-        print("Members in channel: " + str(len(message.channel.members)))
+        logging.info("Starting a new round of Among Us in '{0}'".format(message.channel.guild.name))
+        logging.info("Channel from: '{0}'".format(message.channel))
+        logging.info("Members in channel: {0}".format(len(message.channel.members)))
 
         start = timer()
         await server.new_game()
-        print("Game prepared in: " + str(int((timer() - start) * 1000)) + " milliseconds")
+        logging.info("Game prepared in {0} milliseconds".format(int((timer() - start) * 1000)))
 
-        print("There are " + str(len(server.players)) + " playing")
+        logging.info("There are {0} players".format(len(server.players)))
         await message.channel.send(
-            "There are {0} crewmates and {1} imposters Among Us!".format(str(len(server.crewmates)),
-                                                                         str(len(server.imposters))))
+            "There are {0} crewmates and {1} imposters Among Us!".format(len(server.crewmates),
+                                                                         len(server.imposters)))
 
 
 @client.event
 async def on_guild_join(server):
-    print("Joined a new guild: " + str(guild))
+    logging.info("Joined a new guild '{0}'".format(guild))
     g = guild.Guild(server)
     guilds[server.id] = g
     await g.setup()
-    print("Guild loaded and setup")
+    logging.info("Guild loaded and setup")
 
 token_file = open("bot_token.txt")
 res = token_file.read()
+logging.info("Token file read, is empty? {0}".format(res.strip() is None))
 
 # Blocking function call, nothing will run after this
 client.run(res)
--- guild.py
+++ guild.py
@@ -1,83 +1,94 @@
 import random
+import logging
+import asyncio
 
 import discord
-import asyncio
 
 
 class Guild:
-    players = []
-    imposters = []
-    crewmates = []
-    guild = None
-    imposter_role = None
-    crewmate_role = None
-    among_us_role = None
-    setup_done = False
 
     def __init__(self, guild):
         self.guild = guild
+        self.players = []
+        self.imposters = []
+        self.crewmates = []
+        self.imposter_role = None
+        self.crewmate_role = None
+        self.among_us_role = None
+        self.setup_done = False
 
     async def setup(self):
         if self.setup_done:
             return
 
-        print("\t\t==== Setting up: " + self.guild.name + " ====")
+        logging.info("\t\t==== Setting up {0} ====".format(self.guild.name))
 
         for r in self.guild.roles:
             if r.name == "Imposter":
-                print("Found Imposter role: " + str(r) + " id: " + str(r.id))
+                logging.info("Found Imposter role: {0} id: {1}".format(r, r.id) + str(r.id))
                 self.imposter_role = r
             elif r.name == "Crewmate":
-                print("Found Crewmate role: " + str(r) + " id: " + str(r.id))
+                logging.info("Found Crewmate role: {0} id: {1}".format(r, r.id) + str(r.id))
                 self.crewmate_role = r
             elif r.name == "among us":
-                print("Found Among Us role: " + str(r) + " id: " + str(r.id))
+                logging.info("Found Among Us role: {0} id: {1}".format(r, r.id) + str(r.id))
                 self.among_us_role = r
 
         if self.among_us_role is None:
-            print("No Among Us role to use, ignoring request")
+            logging.error("No Among Us role to use, ignoring request")
             return
 
         # Create the roles if they don't exist
         if self.imposter_role is None:
-            print("Creating imposter role")
+            logging.info("Creating imposter role")
             self.imposter_role = await self.guild.create_role(name="Imposter", color=discord.colour.Color.dark_red())
         if self.crewmate_role is None:
-            print("Creating crewmate role")
+            logging.info("Creating crewmate role")
             self.crewmate_role = await self.guild.create_role(name="Crewmate", color=discord.colour.Color.dark_blue())
 
         self.find_players()
         self.setup_done = True
 
     async def new_game(self):
-        self.find_players()
-        num_imposters = int(len(self.players) * 0.2)
-        if num_imposters < 1:
-            num_imposters = 1
+        try:
+            self.find_players()
+            num_imposters = int(len(self.players) * 0.2)
+            if num_imposters < 1:
+                num_imposters = 1
 
-        # reset the roles of the previous imposters
-        if len(self.imposters) > 0:
-            await asyncio.gather(*(m.add_roles(self.crewmate_role) for m in self.imposters))
-            await asyncio.gather(*(m.remove_roles(self.imposter_role) for m in self.imposters))
-        else:
-            await asyncio.gather(*(m.add_roles(self.crewmate_role) for m in self.players))
+            # reset the roles of the previous imposters
+            if len(self.imposters) > 0:
+                logging.info("Resetting roles of imposters from previous round")
+                await asyncio.gather(*(m.add_roles(self.crewmate_role) for m in self.imposters))
+                await asyncio.gather(*(m.remove_roles(self.imposter_role) for m in self.imposters))
+            else:
+                await asyncio.gather(*(m.add_roles(self.crewmate_role) for m in self.players))
 
-        random.shuffle(self.players)
-        self.imposters.clear()
-        self.crewmates.clear()
-        await asyncio.gather(*(m.add_roles(self.imposter_role) for m in self.players[:num_imposters]))
+            random.shuffle(self.players)
+            self.imposters.clear()
+            self.crewmates.clear()
+            logging.info("Assigning imposter role to new imposters")
+            await asyncio.gather(*(m.add_roles(self.imposter_role) for m in self.players[:num_imposters]))
 
-        for m in self.players[:num_imposters]:
-            self.imposters.append(m)
-        for m in self.players[num_imposters:]:
-            self.crewmates.append(m)
+            logging.info("Updating internal cache of imposters and crewmates")
+            for m in self.players[:num_imposters]:
+                self.imposters.append(m)
+            for m in self.players[num_imposters:]:
+                self.crewmates.append(m)
+        except discord.HTTPException:
+            logging.error("Failed to start a new game")
 
     def find_players(self):
-        for m in self.guild.members:
-            if self.among_us_role in m.roles and m not in self.players:
-                self.players.append(m)
+        self.players.clear()
+        self.crewmates.clear()
+        self.imposters.clear()
 
-            if self.crewmate_role in m.roles and m not in self.crewmates:
+        logging.info("Searching all {0} guild members for their roles".format(len(self.guild.members)))
+        for m in self.guild.members:
+            if self.among_us_role in m.roles:
+                self.players.append(m)
+            if self.crewmate_role in m.roles:
                 self.crewmates.append(m)
-            if self.imposter_role in m.roles and m not in self.imposters:
+            if self.imposter_role in m.roles:
                 self.imposters.append(m)
+        logging.info("{0} among us players processed".format(len(self.players)))
